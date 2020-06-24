eb3c38845b444f114335a2ee7367a7b28417500f
Muhammad Yaqub <zakuby@gmail.com>
2020-06-24 14:32:15 +0700

    initial commit

---
+++ app/src/main/java/org/moviedb/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.moviedb.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.moviedb.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
